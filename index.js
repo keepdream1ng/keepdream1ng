720ecd2fd4c84d4be5e620b49a3422806aaf8271
Anatolii Ivanov <gotnickname@gmail.com>
2023-09-21 11:24:45 +0530

    added counter function in the end of DOM load

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ function setRandomBG_Hue(){
     const randomHue = Math.random() * 360;
     document.documentElement.style.setProperty('--BGCOLOR-HUE', randomHue);
 }
+
+function setCssCounterForElements(htmlTag, startCounter){
+    let elements = Array.from(document.getElementsByTagName(htmlTag));
+    elements.forEach((item) => {
+        item.style.setProperty('--counter', startCounter++);
+    });
+}
