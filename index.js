592a572a7c56c1ddbdb8cacb3a84d3abbc96c976
Anatolii Ivanov <gotnickname@gmail.com>
2023-09-21 15:51:33 +0530

    colorized

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-function setRandomBG_Hue(){
-    const randomHue = Math.random() * 360;
-    document.documentElement.style.setProperty('--BGCOLOR-HUE', randomHue);
-}
-
-function setCssCounterForElements(cssSelectors){
-    passFuncToSelectedBasedOnOrderI(cssSelectors, (I, item) =>{
-        item.style.setProperty('--counter', I);
-    });
-}
 
 function passFuncToSelectedBasedOnOrderI(cssSelectors, func){
     let elements = document.querySelectorAll(cssSelectors);
     for (let I = 0; I < elements.length; I++){
         func(I, elements[I]);
     }
+}
+
+// color settings for dynamic hsl pallet
+const initialHue = Math.random() * 360;
+const stepOfHue = 10;
+
+const setRandomBG_Hue = function(){
+    document.documentElement.style.setProperty('--BGCOLOR-HUE', initialHue);
+}
+
+const setHeadersStickyAndColourfull = function(){
+    passFuncToSelectedBasedOnOrderI("main h1, main h2", (I, item) =>{
+        item.style.cssText = `
+            position: sticky;
+            top: calc(${I} * 2rem);
+            z-index: 1;
+            background-color: hsl(${initialHue + stepOfHue * I}, var(--MAIN-SAT), var(--MAIN-LIGHT));
+        `;
+    });
+}
+
+const setSectionsColourfull = function(){
+    passFuncToSelectedBasedOnOrderI("main section", (I, item) =>{
+        item.style.cssText = `
+            background-color: hsl(${initialHue + stepOfHue * (I + 1)}, var(--MAIN-SAT), var(--MAIN-LIGHT));
+        `;
+    });
 }
